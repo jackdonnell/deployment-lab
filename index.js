8166dc48a414b413df3ebc585fca32ba2a3d673e
Jack <jackdonnell74@gmail.com>
2022-07-21 03:47:24 +0700

    8

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
+const port = process.env.PORT || 4005
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './index.html'))
 })
 
-const port = process.env.PORT || 4005
 
 app.listen(port, () => {
     console.log(`server is running on ${port}`)
