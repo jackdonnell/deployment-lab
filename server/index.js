0f755f50e0a30affbcd7db9246c85bb28f3c4a1b
Jack <jackdonnell74@gmail.com>
2022-07-21 03:37:23 +0700

    6

--- server/index.js
+++ server/index.js
@@ -7,10 +7,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './index.html'))
 })
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../server/index.js'))
-})
-
 const port = process.env.PORT || 4005
 
 app.listen(port, () => {
